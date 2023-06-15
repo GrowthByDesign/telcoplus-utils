d844724df4c167b25415a9e8f66a1d1ea83caaf0
Dillon Ellis <dillonzellis@gmail.com>
2023-06-15 14:40:09 -0400

    make hero text smaller

--- app/hero/Hero.tsx
+++ app/hero/Hero.tsx
@@ -8,10 +8,8 @@ const Hero = () => {
     <div className="tw-relative">
       <Container className="tw-absolute tw-left-2/4 tw-z-10 tw-mx-auto tw-flex tw-h-full tw--translate-x-2/4 tw-flex-col tw-items-center tw-justify-around md:tw-flex-row lg:tw-justify-between">
         <div className="tw-flex tw-flex-col tw-gap-2 tw-text-white lg:tw-gap-4">
-          <h1 className="tw-text-3xl tw-font-bold md:tw-text-5xl lg:tw-text-7xl">
-            EARN MORE
-          </h1>
-          <h2 className="tw-pb-4 tw-text-2xl tw-font-semibold md:tw-text-3xl lg:tw-pb-8 lg:tw-text-5xl">
+          <h1 className="tw-text-xl tw-font-bold">EARN MORE</h1>
+          <h2 className="tw-pb-4 tw-text-2xl tw-font-bold md:tw-text-4xl">
             High CD Rates!
           </h2>
           <Button text="learn more" link="https://www.telcoplus.org/rates/" />
--- app/hero/HeroImg.tsx
+++ app/hero/HeroImg.tsx
@@ -6,7 +6,7 @@ const HeroImg = () => {
     <div className="home-hero-img tw-bg-brandingBlue-400">
       <img
         className="tw-mx-auto tw-min-h-[375px] tw-w-full tw-max-w-[1920px] tw-border-brandingBlue-500 tw-object-cover md:tw-min-h-[500px] xxxl:tw-border-l-4 xxxl:tw-border-r-4"
-        src="https://www.telcoplus.org/wp-content/uploads/2023/06/hero-6-23-min.jpg"
+        src="https://www.telcoplus.org/wp-content/uploads/2023/06/ballon-hero.jpg"
         alt="hero image"
       />
     </div>
