ce5bcbe0da7ed517f2fa80d46109f0dce48cf457
dillonzellis <dillonzellis@gmail.com>
2023-07-07 14:47:18 -0400

    update repos

--- app/(pages)/digital-branch/for-sale/data.ts
+++ app/(pages)/digital-branch/for-sale/data.ts
@@ -79,13 +79,4 @@ export const repos: Repo[] = [
     startingPrice: "$21,995",
     buyPrice: "$25,995",
   },
-  {
-    imgSrc: "https://www.telcoplus.org/wp-content/uploads/2023/06/sonata.jpg",
-    title: "2020 Hyundai Sonata",
-    engine: "4D, SEL 2.5L I4",
-    mileage: "27,131 miles, in Tyler",
-    color: "Red Exterior",
-    startingPrice: "$21,995",
-    buyPrice: "$24,995",
-  },
 ];
