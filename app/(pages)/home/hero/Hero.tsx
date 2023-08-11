de42ff4112e9daf5132d7a732f1303410e8e1c55
dillonzellis <dillonzellis@gmail.com>
2023-08-11 12:54:20 -0400

    update home page hero

--- app/(pages)/home/hero/Hero.tsx
+++ app/(pages)/home/hero/Hero.tsx
@@ -7,12 +7,10 @@ const Hero = () => {
   return (
     <div className="tw-relative">
       <Container className="tw-absolute tw-left-2/4 tw-z-10 tw-mx-auto tw-flex tw-h-full tw--translate-x-2/4 tw-flex-col tw-items-center tw-justify-around md:tw-flex-row lg:tw-justify-between">
-        <div className="tw-flex tw-flex-col tw-gap-2 tw-text-white lg:tw-gap-4">
-          <h1 className="tw-text-xl tw-font-bold">EARN MORE</h1>
-          <h2 className="tw-pb-4 tw-text-2xl tw-font-bold md:tw-text-4xl">
-            High CD Rates!
-          </h2>
-          <Button text="learn more" link="https://www.telcoplus.org/rates/" />
+        <div className="tw-flex tw-flex-col tw-gap-2 tw-text-white">
+          <h1 className="tw-text-xl tw-font-medium">Third Location</h1>
+          <h2 className="tw-pb-4 tw-text-2xl tw-font-bold md:tw-text-4xl tw-italic">For More Convenience</h2>
+          <Button text="visit" link="https://www.telcoplus.org/about/#locations" />
         </div>
         <Login />
       </Container>
