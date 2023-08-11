509cd7878d50f5f9f16d5a8681172e097faf769b
dillonzellis <dillonzellis@gmail.com>
2023-08-11 15:08:33 -0400

    add broadway location

---
+++ app/(pages)/about/locations/(branches)/TylerBroadway.tsx
@@ -0,0 +1,39 @@
+import Container from "@/app/(components)/Container";
+import LocationImg from "../LocationImg";
+import ContentSection from "@/app/(components)/ContentSection";
+import TwoCol from "@/app/(components)/TwoCol";
+import LocationsCol from "../LocationsCol";
+import LinkText from "@/app/(components)/LinkText";
+import LocationColWrapper from "../LocationColWrapper";
+
+const TylerBroadway = () => {
+  return (
+    <ContentSection>
+      <Container>
+        <TwoCol>
+          <LocationImg imgSrc="https://www.telcoplus.org/wp-content/uploads/2023/03/TelcoPlusCU_Tyler-037-Edit-Edit-1.jpg" imgAlt="Broadway branch" />
+          <LocationColWrapper>
+            <LocationsCol title="Tyler - Broadway" largeTitle>
+              <div>7395 S Broadway Ave</div>
+              <div>Tyler, TX 75703</div>
+              <div>Phone: <LinkText label="903-597-4321" href="tel:+1903-597-4321" /> or <LinkText label="800-492-7283" href="tel:+1800-492-7283" /></div>
+              <div>Fax: 903-534-3853</div>
+            </LocationsCol>
+            <LocationsCol title="Lobby Hours">
+              <div>Mon/Tue/Wed: 9:00 a.m. - 5:30 p.m.</div>
+              <div>Thu/Fri: 9:00 a.m. - 6:00 p.m.</div>
+              <div>Sat: 9:00 a.m. - 12 p.m.*</div>
+            </LocationsCol>
+            <LocationsCol title="Drive-Thru Hours">
+              <div>Mon/Tues/Wed: 8:00 a.m. - 5:30 p.m.</div>
+              <div>Thu/Fri: 8:00 a.m. - 6:00 p.m.</div>
+              <div>Sat: 9:00 a.m. - 12:00 p.m.*</div>
+            </LocationsCol>
+          </LocationColWrapper>
+        </TwoCol>
+      </Container>
+    </ContentSection>
+  )
+}
+
+export default TylerBroadway;
--- app/(pages)/about/locations/page.tsx
+++ app/(pages)/about/locations/page.tsx
@@ -1,11 +1,11 @@
-import Container from "@/app/(components)/Container";
 import MainBranch from "./(branches)/MainBranch";
-import ContentSection from "@/app/(components)/ContentSection";
+import TylerBroadway from "./(branches)/TylerBroadway";
 
 const Locations = () => {
   return (
     <div>
       <MainBranch />
+      <TylerBroadway />
       <div className="tw-text-lg tw-font-semibold">*Closed on Saturdays that fall in a 3 day holiday weekend schedule. Please see Closed Schedule.</div>
     </div >
   )
