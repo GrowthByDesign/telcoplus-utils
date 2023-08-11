d424d87ddee6ab828239a74ef0239b24a304922a
dillonzellis <dillonzellis@gmail.com>
2023-08-11 14:53:57 -0400

    change two col to handle reverse and make it flex

--- app/(components)/TwoCol.tsx
+++ app/(components)/TwoCol.tsx
@@ -1,12 +1,16 @@
 type TwoColProps = {
   children: React.ReactNode;
+  reverse?: boolean,
 };
 
-const TwoCol = ({ children }: TwoColProps) => {
+const TwoCol = ({ children, reverse }: TwoColProps) => {
+
+  const reverseClass = reverse ? "tw-flex-col-reverse" : ""
+
   return (
-    <div className="tw-grid tw-items-center tw-gap-8 md:tw-gap-12 lg:tw-grid-cols-2 lg:tw-gap-20">
+    <div className={`tw-flex tw-flex-col lg:tw-grid lg:tw-items-center tw-gap-8 md:tw-gap-12 lg:tw-grid-cols-2 lg:tw-gap-20 ${reverseClass} `.trim()} >
       {children}
-    </div>
+    </div >
   );
 };
 
