2123e32cffe7590c7bf1db35dca2daddc701c046
dillonzellis <dillonzellis@gmail.com>
2023-09-01 15:30:51 -0400

    add banking promo

--- app/(pages)/banking/page.tsx
+++ app/(pages)/banking/page.tsx
@@ -1,20 +1,21 @@
-import Promo from "./Promo"
+import Promo from "./Promo";
 
 const BankingPage = () => {
-  return <div>
-    <div>Banking Page</div>
-    <Promo
-      imgSrc="https://www.telcoplus.org/wp-content/uploads/2023/08/New-Branch-CD-website.jpg"
-      imgAlt="promo rates"
-      disclaimer="*APY=Annual Percentage Yield. CD=Share Certificate. Available to businesses as well as individuals.
+  return (
+    <div>
+      <div>Banking Page</div>
+      <Promo
+        imgSrc="https://www.telcoplus.org/wp-content/uploads/2023/09/New-Branch-CD-website-sold-out.jpg"
+        imgAlt="promo rates"
+        disclaimer="*APY=Annual Percentage Yield. CD=Share Certificate. Available to businesses as well as individuals.
     Minimum $25,000. No maximum. Offer Aug. 28 - Sept. 16, 2023. Offer to end when each CD term
     reaches 2 million or offer date ends! NEW MONEY ONLY. Only available at the new branch. Retain dividends.
     Option 1. 8-month term; Converts to regular 6-mo certificate at maturity; 90-day EWD penalty.
     Option 2. 11-month term; Converts to regular 12-mo certificate at maturity; 180-day EWD penalty.
-    Option 3. 15-month term; Converts to regular 12-mo certificate at maturity; 180-day EWD penalty.
-    Federally Insured by the NCUA.  903-508-2570"
-    />
-  </div>
-}
+    Option 3. 14-month term, SOLD OUT. Federally Insured by the NCUA.  903-508-2570"
+      />
+    </div>
+  );
+};
 
-export default BankingPage
+export default BankingPage;
