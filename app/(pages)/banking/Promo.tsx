e1a1fba04f80bf7dd4eb7394b5c568cfd8789c8a
dillonzellis <dillonzellis@gmail.com>
2023-09-13 11:40:15 -0400

    update banking promos

--- app/(pages)/banking/Promo.tsx
+++ app/(pages)/banking/Promo.tsx
@@ -1,32 +1,33 @@
-import Container from "@/app/(components)/Container"
-import Disclaimer from "@/app/(components)/Disclaimer"
-
-{/* <div class="[ py-1 md-py-2 xl-pb-5 xl-pt-15 ]"> */ }
-{/*   <div class="[ container-de ]"> */ }
-{/*     <div class="[ grid ] [ row-gap-2 gap-2 md-gap-3 ] [ md-grid-cols-2 ] [ lg-gap-5 ] [ items-center ]"> */ }
-{/*       <div class="[ img-col ]"><img class="[ md-max-mx-auto md-ml-auto ] [ b-rad ] [ content-shadow-left ]" src="https://www.telcoplus.org/wp-content/uploads/2023/06/July-23-CD-website.jpg" alt="" srcset="" /></div> */ }
-{/*     </div> */ }
-{/*   </div> */ }
-{/* </div> */ }
+import Container from "@/app/(components)/Container";
+import Disclaimer from "@/app/(components)/Disclaimer";
 
 type PromoProps = {
-  imgSrc: string
-  imgAlt: string
-  disclaimer?: string
-}
+  imgSrc?: string;
+  imgAlt?: string;
+  disclaimer?: string;
+};
 
 const Promo = ({ imgSrc, imgAlt, disclaimer }: PromoProps) => {
-
   return (
     <div className="promo-wrapper tw-py-4 md:tw-py-8 xl:tw-pb-20 xl:tw-pt-60">
       <Container>
-        <div className={disclaimer ? "tw-space-y-8" : ""}>
-          <img src={imgSrc} alt={imgAlt} className="b-rad tw-mx-auto content-shadow-left" />
-          {disclaimer && <div className="tw-max-w-prose tw-mx-auto"><Disclaimer>{disclaimer}</Disclaimer></div>}
-        </div>
+        {imgSrc && (
+          <div className={disclaimer ? "tw-space-y-8" : ""}>
+            <img
+              src={imgSrc}
+              alt={imgAlt}
+              className="b-rad content-shadow-left tw-mx-auto"
+            />
+            {disclaimer && (
+              <div className="tw-mx-auto tw-max-w-prose">
+                <Disclaimer>{disclaimer}</Disclaimer>
+              </div>
+            )}
+          </div>
+        )}
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Promo
+export default Promo;
--- app/(pages)/banking/page.tsx
+++ app/(pages)/banking/page.tsx
@@ -4,16 +4,7 @@ const BankingPage = () => {
   return (
     <div>
       <div>Banking Page</div>
-      <Promo
-        imgSrc="https://www.telcoplus.org/wp-content/uploads/2023/09/New-Branch-CD-website-sold-out.jpg"
-        imgAlt="promo rates"
-        disclaimer="*APY=Annual Percentage Yield. CD=Share Certificate. Available to businesses as well as individuals.
-    Minimum $25,000. No maximum. Offer Aug. 28 - Sept. 16, 2023. Offer to end when each CD term
-    reaches 2 million or offer date ends! NEW MONEY ONLY. Only available at the new branch. Retain dividends.
-    Option 1. 8-month term; Converts to regular 6-mo certificate at maturity; 90-day EWD penalty.
-    Option 2. 11-month term; Converts to regular 12-mo certificate at maturity; 180-day EWD penalty.
-    Option 3. 14-month term, SOLD OUT. Federally Insured by the NCUA.  903-508-2570"
-      />
+      <Promo imgSrc="" imgAlt="" disclaimer="" />
     </div>
   );
 };
