81a8472eaa4cfa4e29674f3865b532646eb765d1
dillonzellis <dillonzellis@gmail.com>
2023-10-02 09:34:13 -0400

    start contacts page

---
+++ app/(pages)/digital-branch/contacts/page.tsx
@@ -0,0 +1,16 @@
+import ContentSection from "@/app/(components)/ContentSection";
+import Container from "@/app/(components)/Container";
+import SectionHeader from "@/app/(components)/SectionHeader";
+
+const ContactsPage = () => {
+  return (
+    <ContentSection>
+      {/* <SectionHeader title="" subHeading="" /> */}
+      <Container>
+        <div>Phone numbers</div>
+      </Container>
+    </ContentSection>
+  );
+};
+
+export default ContactsPage;
