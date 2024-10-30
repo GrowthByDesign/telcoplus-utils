12dcc3bde9d9d383b7a812543d9cdb8217abb91a
cameornc81 <cameronc@growthbydesign.org>
2024-10-30 13:37:07 -0400

    repos updated

--- app/(pages)/digital-branch/for-sale/data.ts
+++ app/(pages)/digital-branch/for-sale/data.ts
@@ -13,16 +13,6 @@ export type Repo = {
 export const repos: Repo[] = [
   {
     imgSrc:
-      "https://www.telcoplus.org/wp-content/uploads/2024/09/2016-sierra.jpg",
-    title: "2016 GM Sierra 1500 Extended Cab",
-    engine: "4WD 5.3L V8",
-    mileage: "155,519 miles",
-    color: "Black",
-    startingPrice: "$17,995",
-    buyPrice: "$19,995",
-  },
-  {
-    imgSrc:
       "https://www.telcoplus.org/wp-content/uploads/2024/09/2015-dodge-challenger-scaled.jpg",
     title: "2015 Dodge Challenger",
     engine: "2D coupe, SXT 3.6L V6",
@@ -30,26 +20,6 @@ export const repos: Repo[] = [
     color: "Navy Blue",
     startingPrice: "$13,995",
     buyPrice: "$15,000",
-  },
-  {
-    imgSrc:
-      "https://www.telcoplus.org/wp-content/uploads/2024/07/chevy-malibu20-scaled.jpg",
-    title: "2020 Chevy Malibu",
-    engine: "4D LT 1.5L",
-    mileage: "83,936 miles",
-    color: "Black",
-    startingPrice: "",
-    buyPrice: "$16,250",
-  },
-  {
-    imgSrc:
-      "https://www.telcoplus.org/wp-content/uploads/2024/06/2014-dodge-durango.jpg",
-    title: "2014 Dodge Durango",
-    engine: "3.6L V6",
-    mileage: "137,500 miles",
-    color: "Black",
-    startingPrice: "$8,995",
-    buyPrice: "$10,000",
   },
   {
     imgSrc:
@@ -63,46 +33,6 @@ export const repos: Repo[] = [
   },
   {
     imgSrc:
-      "https://www.telcoplus.org/wp-content/uploads/2024/05/2014-kia-optima.jpg",
-    title: "2014 KIA Optima",
-    engine: "4D LX 2.4L I4",
-    mileage: "152,490 miles",
-    color: "Black",
-    startingPrice: "$4,000",
-    buyPrice: "$5,699",
-  },
-  {
-    imgSrc:
-      "https://www.telcoplus.org/wp-content/uploads/2024/05/2015-dodge-ram.jpg",
-    title: "2015 Ram 1500 Crew Cab SLT",
-    engine: "4WD 5.7L V8",
-    mileage: "144,516 miles",
-    color: "Gray",
-    startingPrice: "$14,995",
-    buyPrice: "$16,995",
-  },
-  {
-    imgSrc:
-      "https://www.telcoplus.org/wp-content/uploads/2024/05/2017-ford-explorer.jpg",
-    title: "2017 Ford Explorer XLT",
-    engine: "Eco Boost 2.3L I4 Turbo",
-    mileage: "104,570 miles",
-    color: "Black",
-    startingPrice: "$12,000",
-    buyPrice: "$14,500",
-  },
-  {
-    imgSrc:
-      "https://www.telcoplus.org/wp-content/uploads/2024/05/2017-ford-f150.jpg",
-    title: "2017 Ford F150 Crew Cab XLT",
-    engine: "Eco Boost, 3.5L V6 Turbo",
-    mileage: "144,613 miles",
-    color: "White",
-    startingPrice: "$19,995",
-    buyPrice: "$21,195",
-  },
-  {
-    imgSrc:
       "https://www.telcoplus.org/wp-content/uploads/2024/05/2018-toyota-camry.jpg",
     title: "2018 Toyota Camry",
     engine: "4D LE 2.5L I4",
@@ -113,16 +43,6 @@ export const repos: Repo[] = [
   },
   {
     imgSrc:
-      "https://www.telcoplus.org/wp-content/uploads/2024/05/2013-nissan-altima.jpg",
-    title: "2013 Nissan Altima",
-    engine: "4D SL 2.5L I4",
-    mileage: "212,076 miles",
-    color: "White",
-    startingPrice: "$3,500",
-    buyPrice: "$4,995",
-  },
-  {
-    imgSrc:
       "https://www.telcoplus.org/wp-content/uploads/2024/03/2016-corvette.jpg",
     title: "2016 Chevrolet Corevette Stingray",
     engine: "2LT, 2D Coupe, 6.2L V8",
@@ -130,16 +50,6 @@ export const repos: Repo[] = [
     color: "Shark Gray Metallic",
     startingPrice: "$40,950",
     buyPrice: "House of Cars, Tyler",
-  },
-  {
-    imgSrc:
-      "https://www.telcoplus.org/wp-content/uploads/2023/06/2018-f-150-edit.jpg",
-    title: "2018 F150 4WD",
-    engine: "Eco Boost, 4WD 2.7L V6 Turbo",
-    mileage: "160,452 miles in Tyler",
-    color: "White Exterior",
-    startingPrice: "$17,950",
-    buyPrice: "$20,500",
   },
 
 ];
