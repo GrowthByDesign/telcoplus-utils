a24e503874ebdda49a88151e368f8f21b292a81a
Anthony B <Abibbi1@wgu.edu>
2025-06-30 10:55:20 -0400

    new update

--- app/(pages)/digital-branch/for-sale/data.ts
+++ app/(pages)/digital-branch/for-sale/data.ts
@@ -43,16 +43,6 @@ export const repos: Repo[] = [
   },
   {
     imgSrc:
-      "https://www.telcoplus.org/wp-content/uploads/2025/06/IMG_2922.jpg",
-    title: "2020 Kia Optima LX - Longview",
-    engine: "2.4L I4",
-    mileage: "77,602 miles",
-    color: "White",
-    startingPrice: "$12,500",
-    buyPrice: "$15,500",
-  },
-  {
-    imgSrc:
       "https://www.telcoplus.org/wp-content/uploads/2025/06/IMG_2921.jpg",
     title: "2020 Kia Sportage - Longview",
     engine: "2.4L ID",
@@ -60,47 +50,6 @@ export const repos: Repo[] = [
     color: "Black",
     startingPrice: "$13,500",
     buyPrice: "$18,500",
-  },
-  {
-    imgSrc:
-
-      "https://www.telcoplus.org/wp-content/uploads/2025/06/2023-polaris-scaled.jpg",
-    title: "2023 Polaris Electric Utility Side by Side - Tyler",
-    engine: "Ranger XP Kinetic Premium 14.9kWh",
-    mileage: "12.7 hours",
-    color: "Black",
-    startingPrice: "",
-    buyPrice: "$17,400",
-  },
-  {
-    imgSrc:
-      "https://www.telcoplus.org/wp-content/uploads/2025/05/IMG_2553.jpg",
-    title: "2015 GMC Yukon SLE - Longview",
-    engine: "5.3L V8",
-    mileage: "242,875",
-    color: "Black",
-    startingPrice: "$8,995",
-    buyPrice: "$11,500",
-  },
-  {
-    imgSrc:
-      "https://www.telcoplus.org/wp-content/uploads/2025/05/IMG_2550.jpg",
-    title: "2016 Ram 1500 SLT - Longview",
-    engine: "3.6L V6",
-    mileage: "189,409",
-    color: "Silver",
-    startingPrice: "$7,550",
-    buyPrice: "$11,500",
-  },
-  {
-    imgSrc:
-      "https://www.telcoplus.org/wp-content/uploads/2025/05/IMG_2424.jpg",
-    title: "2018 F-150 XLT 4WD - Longview",
-    engine: "5.0L V8",
-    mileage: "183,254 miles",
-    color: "Black",
-    startingPrice: "$15,995",
-    buyPrice: "$19,995",
   },
   {
     imgSrc:
@@ -114,16 +63,6 @@ export const repos: Repo[] = [
   },
   {
     imgSrc:
-      "https://www.telcoplus.org/wp-content/uploads/2025/05/2013-f150-crew-cab.jpg",
-    title: "2013 F-150 Crew Cab XLT 4WD - Longview",
-    engine: "5.0L V8 Eco Boost",
-    mileage: "144,870 miles",
-    color: "White",
-    startingPrice: "$14,995",
-    buyPrice: "$17,995",
-  },
-  {
-    imgSrc:
       "https://www.telcoplus.org/wp-content/uploads/2025/05/2010-f350-super-duty.jpg",
     title: "2010 F-350 Super Duty King Ranch - Longview",
     engine: "4WD, 6.4L V8 T-Diesel",
@@ -131,46 +70,6 @@ export const repos: Repo[] = [
     color: "White",
     startingPrice: "$14,995",
     buyPrice: "$17,995",
-  },
-  {
-    imgSrc:
-      "https://www.telcoplus.org/wp-content/uploads/2025/05/2020-kia-optima.jpg",
-    title: "2020 Kia Optima LX - Longview",
-    engine: "2.4L L I4",
-    mileage: "139,331 miles",
-    color: "Silver",
-    startingPrice: "$9,995",
-    buyPrice: "$12,995",
-  },
-  {
-    imgSrc:
-      "https://www.telcoplus.org/wp-content/uploads/2025/04/20250401_111704_resized.jpg",
-    title: "2015 GMC Sierra 1500 SLT 4WD - Longview",
-    engine: "Crew Cab, 5.3L V8",
-    mileage: "164,022 miles",
-    color: "Black",
-    startingPrice: "$16,500",
-    buyPrice: "$19,750",
-  },
-  {
-    imgSrc:
-      "https://www.telcoplus.org/wp-content/uploads/2025/03/20250306_114814.jpg",
-    title: "2018 F150 XLT Turbo 4WD - Longview",
-    engine: "3.5L V6 Eco Boost",
-    mileage: "195,738 miles",
-    color: "Gray",
-    startingPrice: "$15,950",
-    buyPrice: "$17,950",
-  },
-  {
-    imgSrc:
-      "https://www.telcoplus.org/wp-content/uploads/2025/02/20250204_163651-scaled.jpg",
-    title: "2018 Ford F150 XL 4x4WD - Longview",
-    engine: "5.0L V8",
-    mileage: "158,865 miles",
-    color: "White",
-    startingPrice: "$15,950",
-    buyPrice: "$17,950",
   },
   {
     imgSrc:
