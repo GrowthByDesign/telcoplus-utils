2b1bfd7e4e73d13e783d97806bf8b10439d99de4
Anthony B <Abibbi1@wgu.edu>
2026-01-08 11:17:37 -0500

    updated for sale page

--- app/(pages)/digital-branch/for-sale/data.ts
+++ app/(pages)/digital-branch/for-sale/data.ts
@@ -37,60 +37,6 @@ export const other: Others[] =[
 export const repos: Repo[] = [
 
 
-       {  
-    imgSrc:"https://www.telcoplus.org/wp-content/uploads/2025/08/IMG_3902.jpg",
-    title: "2018 Jeep Grand Cherokee 4WD Limited - Longview",
-    engine: "3.6L V6",
-    mileage: "132,902 miles",
-    color: "Maroon",
-    startingPrice: "$12,750",
-    buyPrice: "$14,500",
-  },
-  
-    {  
-    imgSrc:"https://www.telcoplus.org/wp-content/uploads/2025/08/IMG_2559-scaled.jpg",
-    title: "2019 Chevrolet Traverse Utility 4D RS   - Tyler",
-    engine: "2.0LI4Turbo",
-    mileage: "126,501 miles ",
-    color: "Black",
-    startingPrice: "$15,995",
-    buyPrice: "$18,995",
-  },
-     {  
-    imgSrc:"https://www.telcoplus.org/wp-content/uploads/2025/08/20250807_143403_resized.jpg",
-    title: "2016 Chevy Travers LS  - Tyler",
-    engine: "3.6L V6",
-    mileage: "230,908 miles ",
-    color: "silver",
-    startingPrice: "$5,550",
-    buyPrice: "$27,995",
-  },
-       {  
-    imgSrc:"https://www.telcoplus.org/wp-content/uploads/2025/08/20250807_143327_resized.jpg",
-    title: "2022 Volkswagen Atlas SEL  - Tyler",
-    engine: "2.0L I4 Turbo AWD",
-    mileage: "51,933 Miles ",
-    color: "silver",
-    startingPrice: "$27,995",
-    buyPrice: "$27,995",
-  },
-    {  
-    imgSrc:"https://www.telcoplus.org/wp-content/uploads/2025/08/20250725_101704-scaled.jpg",
-    title: "Retro Lobby Chairs - Must pick up Longview Branch, Ask for Crystal $150 for all 8",
-    startingPrice: "$50 ",
-       buyPrice: "",
-
-  },
-
-     {  
-    imgSrc:"https://www.telcoplus.org/wp-content/uploads/2025/07/20250730_135246_resized.jpg",
-    title: "2022 Harley Davidson FLHRXS - Longview",
-    engine: "Road King Special, 1870 CCs 4 stroke 6 speeds 2 cylinders",
-    mileage: " ",
-    color: "",
-    startingPrice: "$14,795",
-    buyPrice: "$17,795",
-  },
     {  
     imgSrc:"https://www.telcoplus.org/wp-content/uploads/2025/07/IMG_3508.jpg",
     title: "2017 Ram 1500 Laramie 4WD - Longview",
@@ -102,16 +48,6 @@ export const repos: Repo[] = [
   },
   {
     imgSrc:
-      "https://www.telcoplus.org/wp-content/uploads/2025/06/20250610_104148-scaled.jpg",
-    title: "2011 Camper Sabre by Forest River - Tyler",
-    engine: "M-32QBTS",
-    mileage: "As is.",
-    color: " ",
-    startingPrice: "$6,000",
-    buyPrice: "$7,500",
-  },
-  {
-    imgSrc:
       "https://www.telcoplus.org/wp-content/uploads/2025/06/IMG_2921.jpg",
     title: "2020 Kia Sportage - Longview",
     engine: "2.4L ID",
@@ -119,36 +55,6 @@ export const repos: Repo[] = [
     color: "Black",
     startingPrice: "$13,500",
     buyPrice: "$18,500",
-  },
-  {
-    imgSrc:
-      "https://www.telcoplus.org/wp-content/uploads/2025/05/2019-gmc-terrain.jpg",
-    title: "2019 GMC Terrain SLE - Longview",
-    engine: "1.5L I4",
-    mileage: "92,680 miles",
-    color: "Black",
-    startingPrice: "$11,500",
-    buyPrice: "$14,995",
-  },
-  {
-    imgSrc:
-      "https://www.telcoplus.org/wp-content/uploads/2025/01/2014-ford-f150.jpg",
-    title: "2014 Ford F150 4WD - Longview",
-    engine: "Crew Cab Limited, ECO Boost 3.5L V6 Turbo",
-    mileage: "126,000 miles",
-    color: "Black",
-    startingPrice: "$17,500",
-    buyPrice: "$19,995",
-  },
-  {
-    imgSrc:
-      "https://www.telcoplus.org/wp-content/uploads/2025/01/2013-ford-f150-scaled.jpg",
-    title: "2013 Ford F150 4WD - Longview",
-    engine: "Crew Cab Limited, ECO Boost 3.5L V6 Turbo",
-    mileage: "149,822 miles",
-    color: "White",
-    startingPrice: "$18,500",
-    buyPrice: "$20,000",
   },
 
 
